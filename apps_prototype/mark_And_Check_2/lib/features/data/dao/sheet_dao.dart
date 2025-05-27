b2355ec94feed0bc9d3afecd382140637b7d2e26
Yasahiro-021 <Yasahiro021@gmail.com>
2025-05-27 15:36:03 +0900

    リファクタリング

--- apps_prototype/mark_And_Check_2/lib/features/data/dao/sheet_dao.dart
+++ apps_prototype/mark_And_Check_2/lib/features/data/dao/sheet_dao.dart
@@ -6,19 +6,33 @@ import '../model/sheet.dart';
 
 final sheetDaoDataSource = appDatabaseManagerProvider;
 final sheetDaoProvider = Provider((ref) => SheetDao(ref));
+
 class SheetDao {
- late final Database db;
+  late final Database db;
+
   SheetDao(Ref ref) {
     db = ref.read(sheetDaoDataSource).db;
   }
 
-  insert(Sheet sheet){
+  insert(Sheet sheet) {
     db.insert("sheets", {"name": sheet.name});
   }
 
-  Future<List<Sheet>> getAll() async{
-    final List<Map<String, dynamic>> rawData = await db.query("sheets");
+
+  Future<List<Sheet>> searchById(int id) async {
+    final List<Map<String, dynamic>> rawData = await db.query(
+      'sheets',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
     List<Sheet> sheets = rawData.map((e) => Sheet.fromJson(e)).toList();
     return sheets;
   }
+
+  Future<List<Sheet>> getAll() async {
+    final List<Map<String, dynamic>> rawData = await db.query('sheets');
+    return rawData.map((e) => Sheet.fromJson(e)).toList();
+  }
+
 }
--- apps_prototype/mark_And_Check_2/lib/features/data/repository/local_repository.dart
+++ apps_prototype/mark_And_Check_2/lib/features/data/repository/local_repository.dart
@@ -4,14 +4,14 @@ import 'package:mark_and_check/features/data/model/sheet.dart';
 
 class LocalRepository {
   late SheetDao dao;
-  late final  sheetsProvider;
+  late final FutureProvider<List<Sheet>> allSheetsProvider;
 
   //コンストラクタでRefを受け取る
   LocalRepository(Ref inputRef) {
     dao = SheetDao(inputRef); //SheetDaoのインスタンスを作成
 
-    //FutureProviderとして、Sheetsの値を提供
-    sheetsProvider = FutureProvider<List<Sheet>>((ref) async {
+    //FutureProviderとしてSheetの配列を提供
+    allSheetsProvider = FutureProvider<List<Sheet>>((ref) async {
       return dao.getAll();
     });
   }
