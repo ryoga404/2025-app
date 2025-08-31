e95405cf1da670ca9259d53de4990f02b7308930
Yasahiro-021 <Yasahiro021@gmail.com>
2025-08-31 09:47:19 +0900

    Nodeのモックを任意に変更

--- web_browser/lib/tree_view/treeview.dart
+++ web_browser/lib/tree_view/treeview.dart
@@ -8,13 +8,14 @@ import 'dart:developer';
 import 'package:web_browser/tree_view/tree_graph.dart';
 
 class TreeView extends HookConsumerWidget {
-  const TreeView({super.key});
 
+  final Node rootNode;
+
+  TreeView({super.key,required this.rootNode});
+  TreeView.mockingNode({super.key}):rootNode = mockedNode(3,3);
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final Node rootNode = mockedNode(2, 3); // 深さ2、子ノード数3のモックノードを生成
+  Widget build(BuildContext context, WidgetRef ref) {; // 深さ2、子ノード数3のモックノードを生成
     log('Root node children count: ${rootNode.children.length}'); // ログを追加
-
     // ノードをNodeWidgetのリストに変換
     final List<Widget> nodeWidgets = rootNode.children
         .map((node) => NodeWidget(node: node))
