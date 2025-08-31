6e0ce1bb9c71206551ffecbc95e94f68e0ad7974
Yasahiro-021 <Yasahiro021@gmail.com>
2025-08-31 20:52:49 +0900

    ホーム画面を仮実装。

--- web_browser/lib/home_page.dart
+++ web_browser/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_browser/router/router.dart';
 import 'package:web_browser/tree_view/treeview.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -8,6 +9,14 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TreeView.mockingNode();
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: ()=>BrowserViewRoute().go(context), child: Text("to browser!")),
+          ElevatedButton(onPressed: ()=>TreeView.mockingNode(), child: Text("to treeview!"))
+        ],
+      ),
+    );
   }
 }
